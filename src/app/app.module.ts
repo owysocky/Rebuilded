1389c575272a4dd6a25685c48ba4acb679a1d1ea
ow <volovykolya@gmail.com>
2019-04-05 15:05:41 -0700

    add buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { BodyComponent } from './body/body.component';
+import { DetailsComponent } from './details/details.component';
+import { AddReviewComponent } from './add-review/add-review.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { BodyComponent } from './body/body.component';
     AppComponent,
     SearchBarComponent,
     NavBarComponent,
-    BodyComponent
+    DetailsComponent,
+    AddReviewComponent
   ],
   imports: [
     BrowserModule
