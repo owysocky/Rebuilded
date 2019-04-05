ac24feb62daea2310224c02f26e1a6bde000550c
ow <volovykolya@gmail.com>
2019-04-05 12:45:24 -0700

    make stars imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BodyComponent } from './body/body.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    NavBarComponent
+    NavBarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
